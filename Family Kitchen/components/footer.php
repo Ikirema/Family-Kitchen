c15f7d6f75882d2ecb8ddb5a77d71d4281f1d699
Ikirema <88835650+Ikirema@users.noreply.github.com>
2022-10-17 21:56:40 +0300

    Add files via upload

---
+++ Family Kitchen/components/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="iankirema@gmail.com">iankirema@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Working hours</h3>
+         <p> 24hrs Services </p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Nairobi, Kenya</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:+254723737728">+254723737728</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Mr. Ian Kirema</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
